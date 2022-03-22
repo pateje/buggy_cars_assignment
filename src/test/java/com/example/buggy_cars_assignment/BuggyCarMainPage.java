de28a3b4b597949d611c5264a1813103ce9782d0
pateje <jetal.patel@outlook.com>
2022-03-22 16:18:56 +1300

    Initial commit

---
+++ src/test/java/com/example/buggy_cars_assignment/BuggyCarMainPage.java
@@ -0,0 +1,12 @@
+package com.example.buggy_cars_assignment;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+// https://www.jetbrains.com/
+public class BuggyCarMainPage {
+    public SelenideElement loginTextbox = $("a.wt-button_mode_primary");
+}
+
